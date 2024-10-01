793aa1fb3b63044f38ce90d2c33c0c18c97adeaa
asmith7939 <jinxed@Korra>
2024-09-30 20:05:04 -0400

    search bar component and example usage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import SearchBar from "./components/searchbar";
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  // example usage of search bar
+  const [query, setQuery] = useState("");
+  function handleUpdate(value) {
+    setQuery(value);
+    console.log(value); // check console for proof it worked
+  }
   return (
     <>
+      <SearchBar
+        placeholder="search for products"
+        onSearch={handleUpdate}
+        searchValue={query}
+      ></SearchBar>
     </>
   );
 }
---
+++ frontend/src/components/searchbar.jsx
@@ -0,0 +1,29 @@
+// Search Bar Component
+/** 
+* This component renders a reusable search bar
+*
+* @param {string} placeholder Dummy text for inside search bar
+* @param {function} onSearch Function call to update value
+* @param {string} className Optional, defaults to string, otherwise class for CSS
+* @param {string} searchValue User input string from search bar
+*/
+import React from "react";
+
+const SearchBar = ({ placeholder, onSearch, className, searchValue }) => {
+  return (
+    <input
+      type="text"
+      className={
+        className ??
+        "text-white font-bold py-2 px-4 rounded-full transition shadow-md"
+      }
+      value={searchValue}
+      placeholder={placeholder}
+      onChange={(e) => {
+        onSearch(e.target.value);
+      }}
+    />
+  );
+};
+
+export default SearchBar;
