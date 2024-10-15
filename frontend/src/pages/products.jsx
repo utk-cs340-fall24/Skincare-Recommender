2acaa86e2003fdd606dc359c3a23efc993789e48
Alan Khalili <khalilialan@gmail.com>
2024-10-15 10:24:10 -0400

    imported auth prompt into products page

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -1,12 +1,14 @@
 {/* This is the products page. */}
 
 import "../../index.css";
+import AuthPrompt from "../components/promptLogin";
 
 function Products() {
-    return (
-        <>
-        <p>this is the products page</p>
-        </>
-    )
+  return (
+    <>
+      <AuthPrompt />
+      <p>this is the products page</p>
+    </>
+  );
 }
 export default Products;
