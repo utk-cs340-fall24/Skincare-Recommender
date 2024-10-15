733e43e9a9cc429e5eaacb48add876d68d5812c5
Kelly Luong <kellyluong06@gmail.com>
2024-10-14 23:54:33 -0400

    Added necessary links for pages

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -1,10 +1,12 @@
 {/* This is the products page. */}
 
 import "../../index.css";
+import NavBar from "../components/navbar.jsx";
 
 function Products() {
     return (
         <>
+        <NavBar />
         <p>this is the products page</p>
         </>
     )
--- frontend/src/pages/quiz.jsx
+++ frontend/src/pages/quiz.jsx
@@ -1,10 +1,12 @@
 {/* This is the quiz page. */}
 
 import "../../index.css";
+import NavBar from "../components/navbar.jsx";
 
 function Quiz() {
     return (
         <>
+        <NavBar />
         <p>this is the quiz page</p>
         </>
     )
